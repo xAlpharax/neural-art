4093f7d550e8e43ec88070139015f7d9023898cb
xAlpharax <42233094+xAlpharax@users.noreply.github.com>
2023-02-27 22:44:09 +0200

    Multi-threaded renderer implemented successfully. Better error handling. Minor changes in multiple areas

--- renderer.py
+++ renderer.py
@@ -1,32 +1,55 @@
 #!/bin/python
 
-### very slow step that will be assigned to multiple
-### jobs later in the development of this tool
-
 ### data loading
 
 import numpy as np
 
-image_array = np.load("images.npy", allow_pickle=True)
+# will need some added functionality there for *.npy
+image_array = np.load("images.npy")
 
 ### progress bar
 
 from tqdm import tqdm
+import sys
 
-pbar = tqdm(total = len(image_array))
+if len(sys.argv) == 1:
+    pbar = tqdm(total=len(image_array), miniters=0, smoothing=0)
 
-### rendering of images
+### image rendering from raw data
 
 import matplotlib.pyplot as plt
 
+dpi = 258 # dpi 258 -> 720p ; dpi 387 -> 1080p output image resolution
+
 def render(index):
+    pbar.update(1)
+
     name = 'Output/neural_art_{:04d}.png'.format(index + 1)
 
     plt.axis('off')
     plt.imshow(image_array[index])
-    plt.savefig(name, bbox_inches='tight', pad_inches=0) # dpi 258 -> 720p ; dpi 387 -> 1080p output image resolution
+    plt.savefig(name, dpi=dpi, bbox_inches='tight', pad_inches=0)
     plt.close('all')
 
-for index in range(0, len(image_array)):
-    render(index)
-    pbar.update(1)
+### multi-threading
+
+import os ; n_cores = os.cpu_count() // 2 ;
+
+if len(sys.argv) == 1:
+    from concurrent.futures import ThreadPoolExecutor
+
+    with ThreadPoolExecutor(max_workers=n_cores) as executor:
+        executor.map(render, range(0, len(image_array)))
+
+    exit()
+
+if sys.argv[1] == "--fix":
+    for index in range(0, 32):
+        ### some artifacts may have slipped
+        ### because of the thread pool
+        name = 'Output/neural_art_{:04d}.png'.format(index + 1)
+
+        plt.axis('off')
+        plt.imshow(image_array[index])
+        plt.savefig(name, dpi=dpi, bbox_inches='tight', pad_inches=0)
+        plt.close('all')
--- stylize.sh
+++ stylize.sh
@@ -1,25 +1,35 @@
-#!/bin/zsh
+#!/bin/bash
 
 ### You must be in the 'neural-art' directory when you run this
 
-if [ $# -eq 0 ]
-  then
+cwd=$(pwd | sed -r 's%.*/%%g')
+if [ $cwd != "neural-art" ] ; then
+    echo "You must be in the 'neural-art' directory when you run this"
+    exit 1
+fi
+
+if [ $# -eq 0 ] ; then
     echo "Style Image and Content Image need to be specified as arguments"
     echo "Example: ./stylize.sh Images/Jitter_Doll.jpg Images/cute.jpg"
     exit 1
 fi
 
-if [ $# -eq 1 ]
-  then
-    echo "Content image needs to be specified"
+if [ $# -eq 1 ] ; then
+    echo "Content image needs to be specified as well"
+    echo "Example: ./stylize.sh Images/Jitter_Doll.jpg Images/cute.jpg"
     exit 1
 fi
+
+./clear_dir.sh
 
 # stylize data [pair (style, content)]
 python neuralart.py $1 $2
 
 # render images (actual frames) from (an) images.npy
-python renderer.py $1 $2
+python renderer.py
+
+# render fix
+python renderer.py --fix
 
 # turn everything into a video
-ffmpeg -framerate 60 -pattern_type glob -i 'Output/neural_art_*.png' -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" ${${2/*\/}/.*}_in_${${1/*\/}/.*}.mp4
+ffmpeg -framerate 60 -pattern_type glob -i 'Output/neural_art_*.png' -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" $(basename ${2%.*})'_in_'$(basename ${1%.*})'.mp4'
